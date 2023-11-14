b257e983a0f231d115814e6c7428a8fcc566e19e
Matt Carter <m@ttcarter.com>
2023-11-14 11:32:35 +1000

    feat: Basic proof-of-concept for $read

--- test/config.js
+++ test/config.js
@@ -1,35 +1,80 @@
 import {createClient as Supabase} from '@supabase/supabase-js'
+import mlog from 'mocha-logger';
 
 /**
 * Base config for tests
 * @type {Object}
 */
 let config = {
-	supabaseUrl: 'FIXME',
-	supabaseKey: 'FIXME',
+	supabaseUrl: 'https://ijnqntoireecgbqmlpka.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqbnFudG9pcmVlY2dicW1scGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA0NDMyNzksImV4cCI6MjAwNjAxOTI3OX0.xacVcYUsyWw3rYybDDln1oDs9xB4LQqv7xDK-8xuyLU',
 	supabaseOptions: {
 		realtime: {
 			// transport: window.WebSocket, // FIXME: Fix for https://github.com/supabase/realtime-js/issues/219#issuecomment-1387158074
 		},
 	},
 
+	table: 'test',
+	idColumn: 'id',
+	dataColumn: 'data',
+	timestampColumn: 'edited_at',
 
+	// Base settings passed when we init Reactive()
+	baseReactive() { return {
+		supabase: config.supabase,
+		table: config.table,
+		idColumn: config.idColumn,
+		debug(...msg) {
+			mlog.log(`[SUPABASE/${this.table}/${this.id}]`, ...msg);
+		},
+	}},
 }
 
 
 /**
 * Create `config.supabase` instance + call reset()
+*
 * @returns {Promise} A promise which resolves when the operation has completed
 */
-export function setup() {
+export async function setup() {
 	config.supabase = Supabase(config.supabaseUrl, config.supabaseKey, config.supabaseOptions);
+	await reset();
 }
 
 
 /**
 * Reset database / table state
+*
+* @returns {Promise} A promise which resolves when the operation has completed
 */
-export function reset() {
+export async function reset() {
+	// Erase all existing data
+	await config.supabase
+		.from(config.table)
+		.delete()
+		.neq('id', '00000000-0000-0000-0000-000000000000') // Have to pass in a filter which will never match to keep Supabase happy when calling .delete()
+
+	// Create some dummy data
+	await config.supabase
+		.from(config.table)
+		.insert([
+			{
+				[config.idColumn]: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+			},
+			{
+				[config.idColumn]: 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb',
+				[config.dataColumn]: {
+					existingKey: 'bbb',
+				}
+			},
+			{
+				[config.idColumn]: 'cccccccc-cccc-cccc-cccc-cccccccccccc',
+				[config.dataColumn]: {
+					existingArray: [1, 2, {three: 3}],
+				}
+			},
+		])
+		.select('id')
 }
 
 
@@ -42,12 +87,6 @@ export function teardown() {
 
 export default {
 	...config,
-
-	baseReactiveSettings: {
-		supabase: config.supabase,
-		table: config.table,
-		idColumn: config.idColumn,
-	},
 
 	// Test utility functions
 	setup, reset, teardown,
--- test/index.js
+++ test/index.js
@@ -7,9 +7,9 @@ describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 	before('supabase setup', config.setup)
 	after('supabase teardown', config.teardown)
 
-	it('basic state checking (purely offline)', ()=> {
-		let state = Reactive({
-			...config.baseReactiveOptions,
+	it('basic state checking (purely offline)', async ()=> {
+		let state = await Reactive({
+			...config.baseReactive(),
 
 			// Purely offline state options
 			read: false,
@@ -27,5 +27,34 @@ describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 			bar: 'Bar!',
 		});
 	});
+
+	it('access an existing row (via settings)', async ()=> {
+		let fetchedA = await Reactive({
+			...config.baseReactive(),
+			id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+		});
+		expect(fetchedA).to.be.an('object');
+		expect(fetchedA).to.have.property('$id', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
+		expect(fetchedA).to.have.property('$timestamp');
+		expect(fetchedA).to.deep.equal({});
+	});
+
+	it('access existing rows (via path)', async ()=> {
+		let fetchedB = await Reactive(`${config.table}/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb`, config.baseReactive())
+		expect(fetchedB).to.be.an('object');
+		expect(fetchedB).to.have.property('$id', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb');
+		expect(fetchedB).to.have.property('$timestamp');
+		expect(fetchedB).to.be.deep.equal({
+			existingKey: 'bbb',
+		});
+
+		let fetchedC = await Reactive(`${config.table}/cccccccc-cccc-cccc-cccc-cccccccccccc`, config.baseReactive())
+		expect(fetchedC).to.be.an('object');
+		expect(fetchedC).to.have.property('$id', 'cccccccc-cccc-cccc-cccc-cccccccccccc');
+		expect(fetchedB).to.have.property('$timestamp');
+		expect(fetchedC).to.be.deep.equal({
+			existingArray: [1, 2, {three: 3}],
+		});
+	})
 
 });
