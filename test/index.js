4ca4f84a3b3b8de2d0d7e81ffb299e391b9863e5
Matt Carter <m@ttcarter.com>
2023-11-14 14:07:48 +1000

    feat: Basic reactivity + watching

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import config from './config.js';
 import {expect} from 'chai';
-import Reactive from '#lib/reactive';
+import Reactive, {defaults as ReactiveDefaults} from '#lib/reactive';
 
 describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 
@@ -13,6 +13,7 @@ describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 
 			// Purely offline state options
 			read: false,
+			watch: false,
 			write: false,
 		});
 
@@ -32,6 +33,8 @@ describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 		let fetchedA = await Reactive({
 			...config.baseReactive(),
 			id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+			watch: false,
+			write: false,
 		});
 		expect(fetchedA).to.be.an('object');
 		expect(fetchedA).to.have.property('$id', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
@@ -39,22 +42,30 @@ describe('@MomsFriendlyDevCo/Supabase-Reactive', ()=> {
 		expect(fetchedA).to.deep.equal({});
 	});
 
-	it('access existing rows (via path)', async ()=> {
-		let fetchedB = await Reactive(`${config.table}/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb`, config.baseReactive())
+	it('access existing rows (via path + options)', async ()=> {
+		let fetchedB = await Reactive(`${config.table}/bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb`, {
+			...config.baseReactive(),
+			watch: false,
+			write: false,
+		})
 		expect(fetchedB).to.be.an('object');
 		expect(fetchedB).to.have.property('$id', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb');
 		expect(fetchedB).to.have.property('$timestamp');
 		expect(fetchedB).to.be.deep.equal({
 			existingKey: 'bbb',
 		});
+	});
 
-		let fetchedC = await Reactive(`${config.table}/cccccccc-cccc-cccc-cccc-cccccccccccc`, config.baseReactive())
+	it('access existing rows (via path + defaults)', async ()=> {
+		Object.assign(ReactiveDefaults, config.baseReactive());
+
+		let fetchedC = await Reactive(`${config.table}/cccccccc-cccc-cccc-cccc-cccccccccccc`);
 		expect(fetchedC).to.be.an('object');
 		expect(fetchedC).to.have.property('$id', 'cccccccc-cccc-cccc-cccc-cccccccccccc');
-		expect(fetchedB).to.have.property('$timestamp');
+		expect(fetchedC).to.have.property('$timestamp');
 		expect(fetchedC).to.be.deep.equal({
 			existingArray: [1, 2, {three: 3}],
 		});
-	})
+	});
 
 });
